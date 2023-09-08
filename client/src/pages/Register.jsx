f0cab120082820cbd4e20e521cc4b3456e60504e
Oryuta13 <ryuta1113abcdol@gmail.com>
2023-09-08 11:28:33 +0900

    validationCheck機能の実装

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,12 +1,19 @@
 import { Box, Button, TextField } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import { LoadingButton } from "@mui/lab";
 import { Link } from "react-router-dom";
 import authApi from "../api/authApi";
 
 const Register = () => {
+  const [usernameErrText, setUsernameErrText] = useState("");
+  const [passwordErrText, setPasswordErrText] = useState("");
+  const [confirmErrText, setConfirmErrText] = useState("");
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setUsernameErrText("");
+    setPasswordErrText("");
+    setConfirmErrText("");
 
     // 入力欄の文字列を取得
     const data = new FormData(e.target);
@@ -16,6 +23,28 @@ const Register = () => {
     console.log(username);
     console.log(password);
     console.log(confirmPassword);
+
+    let error = false;
+
+    if (username === "") {
+      error = true;
+      setUsernameErrText("名前を入力してください");
+    }
+    if (password === "") {
+      error = true;
+      setPasswordErrText("パスワードを入力してください");
+    }
+    if (confirmPassword === "") {
+      error = true;
+      setConfirmErrText("確認用パスワードを入力してください");
+    }
+    if (password !== confirmPassword) {
+      error = true;
+      setConfirmErrText("パスワードと確認用パスワードが一致しません");
+    }
+
+    // もしエラーが起きれば次に進ませない
+    if (error) return;
 
     // 新規登録APIを叩く
     try {
@@ -27,13 +56,25 @@ const Register = () => {
       localStorage.setItem("token", res.token);
       console.log("新規登録に成功しました");
     } catch (err) {
-      console.log(err);
+      const errors = err.data.errors;
+      console.log(errors);
+      errors.forEach((err) => {
+        if (err.path === "username") {
+          setUsernameErrText(err.msg);
+        }
+        if (err.path === "password") {
+          setPasswordErrText(err.msg);
+        }
+        if (err.path === "confirmPassword") {
+          setConfirmErrText(err.msg);
+        }
+      });
     }
   };
 
   return (
     <>
-      <Box component="form" onSubmit={handleSubmit}>
+      <Box component="form" onSubmit={handleSubmit} noValidate>
         <TextField
           fullWidth
           id="username"
@@ -41,6 +82,8 @@ const Register = () => {
           margin="normal"
           name="username"
           required
+          helperText={usernameErrText}
+          error={usernameErrText !== ""}
         />
         <TextField
           fullWidth
@@ -50,6 +93,8 @@ const Register = () => {
           name="password"
           type="password"
           required
+          helperText={passwordErrText}
+          error={passwordErrText !== ""}
         />
         <TextField
           fullWidth
@@ -59,6 +104,8 @@ const Register = () => {
           name="confirmPassword"
           type="password"
           required
+          helperText={confirmErrText}
+          error={confirmErrText !== ""}
         />
         <LoadingButton
           sx={{ mt: 3, mb: 2 }}
