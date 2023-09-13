774c3d3afc04d15836b2f901a58e1b27da68b34b
Oryuta13 <ryuta1113abcdol@gmail.com>
2023-09-13 14:39:42 +0900

    emojiの表示機能の実装

--- client/src/components/common/EmojiPicker.jsx
+++ client/src/components/common/EmojiPicker.jsx
@@ -4,19 +4,47 @@ import Picker from "@emoji-mart/react";
 
 const EmojiPicker = (props) => {
   const [selectedEmoji, setSelectedEmoji] = useState();
+  const [isShowPicker, setIsShowPicker] = useState(false);
 
   // アイコンが変更されるたびに選択された絵文字を更新する
   useEffect(() => {
     setSelectedEmoji(props.icon);
   }, [props.icon]);
 
+  // クリックされるたびtrueとfalseを切り替える
+  const showPicker = () => setIsShowPicker(!isShowPicker);
+
+  const selectEmoji = (e) => {
+    const emojiCode = e.unified.split("-");
+    console.log(emojiCode);
+    let codesArray = [];
+    emojiCode.forEach((el) => codesArray.push("0x" + el));
+    const emoji = String.fromCodePoint(...codesArray);
+    console.log(emoji);
+    // 絵文字を選択したらpickerを閉じる
+    setIsShowPicker(false);
+    props.onChange(emoji);
+  };
+
   return (
     <Box>
-      <Typography variant="h3" fontWeight="700" sx={{ cursor: "pointer" }}>
+      <Typography
+        variant="h3"
+        fontWeight="700"
+        sx={{ cursor: "pointer" }}
+        onClick={showPicker}
+      >
         {selectedEmoji}
       </Typography>
-      <Box sx={{ display: "none" }}>
-        <Picker />
+      {/* isShowPickerがtrueならブロック要素、falseならnone */}
+      <Box
+        sx={{
+          display: isShowPicker ? "block" : "none",
+          position: "absolute",
+          zIndex: "100",
+        }}
+      >
+        <Picker onEmojiSelect={selectEmoji} />
       </Box>
     </Box>
   );
