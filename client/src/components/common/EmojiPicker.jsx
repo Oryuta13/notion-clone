dc4b79f72e8c2164e6cffe71a03b8b1d5240b4ef
Oryuta13 <ryuta1113abcdol@gmail.com>
2023-09-13 12:02:32 +0900

    emoji-pickerの導入

---
+++ client/src/components/common/EmojiPicker.jsx
@@ -0,0 +1,25 @@
+import { Box, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import Picker from "@emoji-mart/react";
+
+const EmojiPicker = (props) => {
+  const [selectedEmoji, setSelectedEmoji] = useState();
+
+  // アイコンが変更されるたびに選択された絵文字を更新する
+  useEffect(() => {
+    setSelectedEmoji(props.icon);
+  }, [props.icon]);
+
+  return (
+    <Box>
+      <Typography variant="h3" fontWeight="700" sx={{ cursor: "pointer" }}>
+        {selectedEmoji}
+      </Typography>
+      <Box sx={{ display: "none" }}>
+        <Picker />
+      </Box>
+    </Box>
+  );
+};
+
+export default EmojiPicker;
